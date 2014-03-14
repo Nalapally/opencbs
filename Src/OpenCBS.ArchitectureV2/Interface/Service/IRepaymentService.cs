3753918f9dfc0e5da607a359504506fb949f6021
Kubanychbek <kuban_aa@mail.ru>
2014-03-14 11:59:27 +0600

    Add repayment service

--- Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
+++ Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using OpenCBS.ArchitectureV2.Service;
+﻿using OpenCBS.ArchitectureV2.Service;
 using OpenCBS.CoreDomain.Contracts.Loans;
 
 namespace OpenCBS.ArchitectureV2.Interface.Service
