9a4143ec3a6877c24a9e5be835364e02c18f5d40
Kubanychbek <kuban_aa@mail.ru>
2014-03-19 12:21:47 +0600

    Add normal repayment script

--- Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
+++ Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
@@ -6,7 +6,12 @@ namespace OpenCBS.ArchitectureV2.Interface.Service
 {
     public interface IRepaymentService
     {
+<<<<<<< HEAD
         Loan Repay();
         Dictionary<string, string> GetAllRepaymentScriptsWithTypes();
+=======
+        Loan Repay(RepaymentConfiguration configuration);
+        Loan RepayAndSave(RepaymentConfiguration configuration);
+>>>>>>> Add normal repayment script
     }
 }
