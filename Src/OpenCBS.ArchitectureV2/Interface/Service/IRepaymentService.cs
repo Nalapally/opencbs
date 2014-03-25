d8164a3bca4168a6fcf7be7fe77cec047288e16f
Evgeniy Abdukarimov <evgeniy.abdukarimov@gmail.com>
2014-03-25 16:47:56 +0600

    Repayment test ANNUITY for normal and late
    AMK

--- Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
+++ Src/OpenCBS.ArchitectureV2/Interface/Service/IRepaymentService.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using OpenCBS.ArchitectureV2.Service;
-﻿using OpenCBS.ArchitectureV2.Service;
 using OpenCBS.CoreDomain.Contracts.Loans;
 
 namespace OpenCBS.ArchitectureV2.Interface.Service
