4e8388491ef84609738828e776d0677b43383223
Kubanychbek <kuban_aa@mail.ru>
2014-04-03 15:36:53 +0600

    Refresh schedule control

--- Src/OpenCBS.ArchitectureV2/Service/Scripts/Repayment/1_NormalRepayment.py
+++ Src/OpenCBS.ArchitectureV2/Service/Scripts/Repayment/1_NormalRepayment.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD:Src/OpenCBS.ArchitectureV2/Service/Scripts/Repayment/1_NormalRepayment.py
 def GetType():
-=======
-def GetDescription():
->>>>>>> Change some logic in repay window:Src/OpenCBS.ArchitectureV2/Service/Scripts/Repayment/NormalRepayment.py
     return "Нормальный"
 
 def RepayCommission(installment):
