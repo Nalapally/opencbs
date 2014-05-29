b75bd7a739137f006d5d75daa848753a21c1be44
Kubanychbek <kuban_aa@mail.ru>
2014-05-29 11:51:53 +0600

    Fix update script 14.8

--- Sql/Database_Update_v14.7.0.0_to_v14.8.0.0.sql
+++ Sql/Database_Update_v14.7.0.0_to_v14.8.0.0.sql
@@ -2,16 +2,15 @@ update Credit
 set interest_rate = cr.interest_rate * FLOOR(365 / (it.nb_of_months*30 + it.nb_of_days))
 from Credit cr
 left join InstallmentTypes it on it.id = cr.installment_type
-go
+GO
 
 update Packages
 set interest_rate = p.interest_rate * FLOOR(365 / (it.nb_of_months*30 + it.nb_of_days)),
 	interest_rate_min = p.interest_rate_min * FLOOR(365 / (it.nb_of_months*30 + it.nb_of_days)),
 	interest_rate_max = p.interest_rate_max * FLOOR(365 / (it.nb_of_months*30 + it.nb_of_days))
 from Packages p
-left join Credit cr on cr.package_id = p.id
-left join InstallmentTypes it on it.id = cr.installment_type
-go
+left join InstallmentTypes it on it.id = p.installment_type
+GO
 
 update ReschedulingOfALoanEvents
 set previous_interest_rate = re.previous_interest_rate * FLOOR(365 / (it.nb_of_months*30 + it.nb_of_days)),
@@ -20,7 +19,7 @@ from ReschedulingOfALoanEvents re
 left join ContractEvents ce on ce.id = re.id
 left join Credit cr on cr.id = ce.contract_id
 left join InstallmentTypes it on it.id = cr.installment_type
-go
+GO
 
 update TrancheEvents
 set interest_rate = tr.interest_rate * FLOOR(365 / (it.nb_of_months*30 + it.nb_of_days))
@@ -28,7 +27,7 @@ from TrancheEvents tr
 left join ContractEvents ce on ce.id = tr.id
 left join Credit cr on cr.id = ce.contract_id
 left join InstallmentTypes it on it.id = cr.installment_type
-go
+GO
 
 UPDATE  [TechnicalParameters]
 SET     [value] = 'v14.8.0.0'
